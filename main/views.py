fe2b06bff2e469c7cd1bdc169b2465d57b4e282a
widaputri <widapk2709@gmail.com>
2024-09-18 09:44:14 +0700

    Update

--- main/views.py
+++ main/views.py
@@ -34,6 +34,7 @@ def show_json(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+# by IDs
 def show_xml_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
